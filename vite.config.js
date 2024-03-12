d5fac4b0ea182ee0ae94a0b27b917d8d14bc231e
AndriiBulhakov <andrijbulgakov5@gmail.com>
2024-03-12 15:40:22 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
-import eslintPlugin from 'vite-plugin-eslint'
 
 // vite.config.js
 export default defineConfig({
-  plugins: [eslintPlugin({ cache: false })],
   server: {
     host: 'localhost',
     cors: '*',
